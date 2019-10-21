f646aaefadd17949aa5e8f77240cac096148e512
Fernando Surenian <fernando.surenian@softvision.com>
2019-10-21 10:13:56 -0300

    Outline always at 2 width

--- src/components/TextInput/TextInputOutlined.js
+++ src/components/TextInput/TextInputOutlined.js
@@ -152,7 +152,7 @@ class TextInputOutlined extends React.Component<ChildTextInputProps, {}> {
             styles.outline,
             {
               borderRadius: theme.roundness,
-              borderWidth: normalizePixelDensity(2),
+              borderWidth: normalizePixelDensity(4),
               borderColor: hasActiveOutline ? activeColor : outlineColor,
             },
           ]}
